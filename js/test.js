d50cbf4fcdc970a1a77db1498a99c9a10f8fb952
Florent Eeckeman <florenteeckeman@live.fr>
2023-01-16 01:03:07 +0100

    first commit

---
+++ js/test.js
@@ -0,0 +1,14 @@
+var e =0
+
+if (1=="1") {
+    if ((1=1,2=2
+    ))
+      (e = 1),
+      console.log('ok1'),
+      console.log('ok2'),
+       console.log('ok3');
+    else
+    console.log('nok1')
+    console.log('nok2')
+      console.log('nok3');
+  }
